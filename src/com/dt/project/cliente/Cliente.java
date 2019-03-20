c025019fbad0d1485d99710ad68ee2f75ae21a67
DiegoVasc <diegosousa.33@hotmail.com>
2019-03-20 09:33:40 -0300

    interface rmi cliente

--- src/com/dt/project/cliente/Cliente.java
+++ src/com/dt/project/cliente/Cliente.java
@@ -10,38 +10,20 @@ public class Cliente {
     public Cliente(){
         System.out.println("Iniciando Cliente....");
         try {
-            msi = (InterfaceRMI) Naming.lookup("rmi://10.180.50.236:1010/ServidorMat_1");
+            msi = (InterfaceRMI) Naming.lookup("rmi://10.180.50.236:1010/Lista");
         } catch (Exception e) {
             System.out.println("Falhou a iniciação do cliente.\n"+e);
             System.exit(0);
         }
     }
     
-    public int[] totalDoVetor(int vet[]) throws RemoteException{
+    public String totalDoVetor(int vet[]) throws RemoteException{
         return msi.vetores(vet);
     }
     
     public static void main(String[] args) {
         Cliente c  = new Cliente();
-        int[] vet = new int[10];
-        int[] vet2 = new int[10];
-        try {
-            for(int i = 0; i <10 ; i++){
-                vet[i] = (int) (Math.random()*10);
-            }
-            vet2 = c.totalDoVetor(vet);
-            System.out.println("\n\n DESORDENADO\n\n");
-            for(int i = 0; i < vet2.length;i++){
-                System.out.println("Valor: \n"+vet[i]);
-            }
-            System.out.println("\n\n ORDENADO\n\n");
-            for(int i = 0; i < vet2.length;i++){
-                System.out.println("Valor: \n"+vet2[i]);
-            }
-            //System.out.println("A soma é: "+c.totalDoVetor(vet));
-        } catch (Exception e) {
-            System.out.println("Excepção  durante chamadas remotas: "+e);
-        }
+      
     }
     private InterfaceRMI msi; 
    
